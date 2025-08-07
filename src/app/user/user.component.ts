7cf66546aa0e3340d53fd3160fd2e49774dc5d2d
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-08-07 15:29:36 -0300

    alterando uso de inputs com required sem uso de signal\

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,15 +6,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
- 
-  //  get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
-  //  }
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
