fee2a8b7ba37bf7f00c1ad225b39f488aedd91d9
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-08-08 11:23:36 -0300

    Feat: Utilizando output helper

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component,EventEmitter, Input, Output, output } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -6,12 +6,17 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({required:true}) id!: string; 
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
