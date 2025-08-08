3c91d146e7ec67943e01d22ce2fa3503478ec7e9
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-08-08 12:09:55 -0300

    Chore: ajustes e criacao de aba users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component,EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -6,11 +6,10 @@ import { Component,EventEmitter, Input, Output, output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required:true}) id!: string; 
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
