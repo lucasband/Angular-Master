f60fa04ed452022235b3c8b3a897c9f874ca9d76
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-08-11 12:08:34 -0300

    Chore: finalizando forms e criando novas tarefas

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,20 +1,32 @@
-import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskModel } from '../task/task.model';
+
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-@Output() cancel = new EventEmitter<void>()
-enteredTitle = signal('');
-enteredSummary = signal('');
-enteredDate = signal('');
+  @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskModel>();
 
-onCancel(){
-this.cancel.emit();
-}
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  onCancel() {
+    this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
+  }
 }
