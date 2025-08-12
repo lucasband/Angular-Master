a34ec7afb36208263869ec0df790df8fd6fdaa6a
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-08-12 09:35:51 -0300

    Feat: criando shared

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,15 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({required: true}) task!: Task;
+  @Input({ required: true }) task!: Task;
   @Output() complete = new EventEmitter<string>();
 
   onCompleteTask() {
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
@@ -20,4 +22,3 @@ export class UserComponent {
     this.select.emit(this.user.id);
   }
 }
- 
