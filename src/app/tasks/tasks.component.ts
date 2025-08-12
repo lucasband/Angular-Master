5d3123d73b7a70d579ab2cb900da980944432c1c
Lucas Emanuel <lukasgomes10@hotmail.com>
2025-08-12 10:08:46 -0300

    Feat: criando service e alterando estruturas

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
-import { NewTaskModel } from './task/task.model';
-
+import { type NewTaskModel } from './task/task.model';
+import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -11,59 +11,24 @@ import { NewTaskModel } from './task/task.model';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) userid!: string;
+  @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUsersTasks() {
-    return this.tasks.filter((task) => task.userId === this.userid);
+    return this.tasksService.getUserTasks(this.userId)
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
+  onCompleteTask(id: string) {}
 
   onStartAddTask(){
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 
-  onAddTask(taskData: NewTaskModel){
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userid,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
-    this.isAddingTask = false;
-  }
 }
